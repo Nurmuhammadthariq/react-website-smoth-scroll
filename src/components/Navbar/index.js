3ccb1187cd79ed916ef99bf8bce5f9a55f4e5954
thoriqnoer <thoriq.noer@gmail.com>
2021-06-17 09:25:03 +0700

    add animate scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaBars } from 'react-icons/fa'
 import {
   Nav,
@@ -11,28 +11,85 @@ import {
   NavBtn,
   NavBtnLink,
 } from './NavbarElements'
+import { animateScroll as scroll } from 'react-scroll'
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false)
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true)
+    } else {
+      setScrollNav(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+  }, [])
+
+  const toggleHome = () => {
+    scroll.scrollToTop()
+  }
+
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to='/'>Dolla</NavLogo>
+          <NavLogo to='/' onClick={toggleHome}>
+            Dolla
+          </NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to='about'>About</NavLinks>
+              <NavLinks
+                to='about'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={10}
+              >
+                About
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to='discover'>Discover</NavLinks>
+              <NavLinks
+                to='discover'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={80}
+              >
+                Discover
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to='services'>Services</NavLinks>
+              <NavLinks
+                to='services'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={80}
+              >
+                Services
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to='signup'>Sign Up</NavLinks>
+              <NavLinks
+                to='signup'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={80}
+              >
+                Sign Up
+              </NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
